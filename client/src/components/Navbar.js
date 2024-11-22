a033d53ad70e915b7fadcab3a071ea691ccdecb2
Umang goyal <goyalumang910@gmail.com>
2024-11-22 20:30:41 +0530

    Changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -91,7 +91,7 @@ const Navbar = () => {
             color={theme.palette.primary.main}
           >
             {/* <Link to="/" color="inherit"> */}
-              LinkUp
+              PostIt
             {/* </Link> */}
           </Typography>
         </HorizontalStack>
