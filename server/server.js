f9a2201c9869d662848991db801aaaf284272648
Umang goyal <goyalumang910@gmail.com>
2024-11-28 22:13:07 +0530

    Arranging server code

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://frontend-link"],
+    origin: ["http://localhost:3000", "https://post-it-frontend.vercel.app"],
     methods: ["POST","GET"],
     credentials: true
   },
