80de03a3b7bdfc7aabc87ce09de9880589224044
Umang goyal <goyalumang910@gmail.com>
2024-11-28 21:46:11 +0530

    Changes for hosting

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ dotenv.config();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "https://post-it-heroku.herokuapp.com"],
+    origin: ["http://localhost:3000", "https://frontend-link"],
+    methods: ["POST","GET"],
+    credentials: true
   },
 });
 
